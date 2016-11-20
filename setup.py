b564185afdae4eb797fb472c0235b32c75180cc7
seba-1511 <seba-1511@hotmail.com>
2016-11-20 15:09:38 -0800

    Setup ready for pypi

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
 
 from setuptools import (
-    setup as install,
-    find_packages,
-)
+        setup as install,
+        find_packages,
+        )
 
-VERSION = '0.0.2'
+VERSION = '0.1.0'
 
 install(
-    name='randopt',
-    version=VERSION,
-    description="Random Search Optimization",
-    long_description=open('README.md').read(),
-    author='Seb Arnold',
-    author_email='smr.arnold@gmail.com',
-    license='License :: OSI Approved :: Apache Software License',
-    packages=find_packages(exclude=["tests"]),
-    classifiers=[
-        'Tools',
-    ],
-    scripts=[
-        'bin/roviz.py',
-    ]
-)
+        name='randopt',
+        version=VERSION,
+        description='Random search optimization and experiment logging. Support async, fancy visualization, distributed execution.',
+        long_description=open('README.md').read(),
+        author='Seb Arnold',
+        author_email='smr.arnold@gmail.com',
+        url = 'https://github.com/seba-1511/randopt',
+        download_url = 'https://github.com/seba-1511/randopt/archive/0.1.0.zip',
+        license='License :: OSI Approved :: Apache Software License',
+        packages=['randopt'],
+        classifiers=[
+            'Tools',
+            ],
+        scripts=[
+            'bin/roviz.py',
+            ]
+        )
