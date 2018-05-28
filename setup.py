7c9b022f8c75d2d04c4755942b69da2554214bb7
seba-1511 <seba-1511@hotmail.com>
2018-05-28 13:52:50 -0700

    Trying to make markdown descriptions work

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ from setuptools import (
         find_packages,
         )
 
-VERSION = '0.1.7'
+VERSION = '0.2.0'
 
 setup(
         name='randopt',
         packages=find_packages(),
         version=VERSION,
-        description='Random search optimization and experiment logging. Support async, fancy visualization, distributed execution.',
+        description='Random search optimization and experiment logging. Support async, visualization, distributed execution.',
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
         author='Seb Arnold',
         author_email='smr.arnold@gmail.com',
         url = 'https://github.com/seba-1511/randopt',
-        download_url = 'https://github.com/seba-1511/randopt/archive/0.1.7.zip',
+        download_url = 'https://github.com/seba-1511/randopt/archive/' + str(VERSION) + '.zip',
         license='License :: OSI Approved :: Apache Software License',
         classifiers=[],
         scripts=[
